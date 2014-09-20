b8c3c6d74f804dd8e6c8c2708d486af1086421f7
Marcos\ Cardoso <marcos.tecnovix@gmail.com>
2014-09-19 22:33:29 -0300

    Devagar eu chego lá

--- app/src/main/java/com/example/ipbfiladelfia/taskers/Taskers.java
+++ app/src/main/java/com/example/ipbfiladelfia/taskers/Taskers.java
@@ -6,6 +6,7 @@ import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -16,6 +17,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -31,11 +33,15 @@ public class Taskers extends Activity
      * Used to store the last screen title. For use in {@link #restoreActionBar()}.
      */
     private CharSequence mTitle;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_taskers);
+
+        Button btAtividades = (Button) findViewById(R.id.btMinhasAtividades);
+        Button btHistorico = (Button) findViewById(R.id.btHistorico);
+
+        btAtividades.setOnClickListener(BotaoFuncao);
 
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getFragmentManager().findFragmentById(R.id.navigation_drawer);
@@ -47,7 +53,19 @@ public class Taskers extends Activity
                 (DrawerLayout) findViewById(R.id.drawer_layout));
     }
 
-    @Override
+    View.OnClickListener BotaoFuncao  = new View.OnClickListener(){
+        public void onClick(View v){
+           switch(v.getId()){
+               case R.id.btMinhasAtividades:
+                   TextView tx = (TextView) findViewById(R.id.txTeste);
+                   tx.setText("Batata");
+                   Intent Atividades = new Intent(Taskers.this,Atividades.class);
+                   startActivity(Atividades);
+                   break;
+           }
+        }
+    };
+
     public void onNavigationDrawerItemSelected(int position) {
         // update the main content by replacing fragments
         FragmentManager fragmentManager = getFragmentManager();
